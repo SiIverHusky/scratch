df6a9a8cd056c7accfdd00d5be6793cd4fc77d57
SiIver Husky <husnainharis2002@gmail.com>
2025-12-23 13:52:20 +0800

    Initial Stages for Progressive Web App support

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'blocks-cache-v1';
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/joystick/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const networkFetch = fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(() => cachedResponse);
+
+      return cachedResponse || networkFetch;
+    })
+  );
+});
